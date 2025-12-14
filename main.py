64a21c94e78e8d1ec5688465553c28d71ea9a317
LandonUsher <lusher3@students.kennesaw.edu>
2025-12-14 14:29:21 -0500

    Final Code
    Final code update and problems solved

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class NutritionTracker:
     def __init__(self):
-        self.Nutrition = []
+        self.nutrition = []
 
-    def add_nutrition(foodname, time, protein, calories):
+    def add_nutrition(self, foodname, time, protein, calories):
         nutrition = {
-            'foodname': food_name,
+            'foodname': foodname,
             'time': time,
             'protein': protein,
             'calories': calories
         }
         self.nutrition.append(nutrition)
-        print(f"Added nutrition: {foodname} on {time} with {protein} grams of protein, including {calories} calories.")
+        print(f"Added nutrition: {foodname} at {time} with {protein} grams of protein, including {calories} calories.")
 
     def view_nutrition(self):
         if not self.nutrition:
@@ -18,17 +18,17 @@ class NutritionTracker:
             return
 
         for i, nutrition in enumerate(self.nutrition, 1):
-            print(f"{i}. {nutrition['name']} on {nutrition['time']} with {nutrition['protein']} grams of protein, and {nutrition['calories']} calories.")
+            print(f"{i}. {nutrition['foodname']} at {nutrition['time']} with {nutrition['protein']} grams of protein, and {nutrition['calories']} calories.")
 
     def delete_nutrition(self, index):
         try:
             removed_nutrition = self.nutrition.pop(index - 1)
-            print(f"Removed nutrients: {removed_nutrition['name']} on {removed_nutrition['time']}.")
+            print(f"Removed nutrients: {removed_nutrition['foodname']} on {removed_nutrition['time']}.")
         except IndexError:
             print("Invalid nutrition number.")
 
 def main():
-    manager = NutritionManager()
+    manager = NutritionTracker()
 
     while True:
         print("\nNutrition Manager")
@@ -39,17 +39,17 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            name = input("Enter Nutrition name: ")
+            foodname = input("Enter Nutrition name: ")
             time = input("Enter intake time (xx:xx): ")
-            Protein = input("Enter the amount of protein: ")
+            protein = input("Enter the amount of protein: ")
             calories = input("Enter the amount calories: ")
-            manager.add_workout(name, date, protein, calories)
+            manager.add_nutrition(foodname, time, protein, calories)
         elif choice == '2':
             manager.view_nutrition()
         elif choice == '3':
             manager.view_nutrition()
             index = int(input("Enter the nutrition number to delete: "))
-            manager.delete_workout(index)
+            manager.delete_nutrition(index)
         elif choice == '4':
             print("Exiting the Nutrion Tracker.")
             break
